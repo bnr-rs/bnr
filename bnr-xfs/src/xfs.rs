a4fee6cb3eab613e233078ba7be07eaebb1e05cf
BNR Rust Developers <157652676+bnr-rs@users.noreply.github.com>
2024-01-24 22:37:04 +0000

    xfs: add `ISO-8559-1` encoding
    Adds the ability to serialize an XML string with the `ISO-8559-1` encoding attribute.

--- bnr-xfs/src/xfs.rs
+++ bnr-xfs/src/xfs.rs
@@ -13,6 +13,7 @@ pub mod xfs_struct;
 
 pub use operation_id::*;
 
+/// Serializes to a XML string with `UTF-8` encoding attribute.
 pub fn to_string<S: serde::Serialize>(s: S) -> Result<String> {
     let mut sink = Vec::with_capacity(0xff);
 
@@ -22,6 +23,21 @@ pub fn to_string<S: serde::Serialize>(s: S) -> Result<String> {
     );
 
     let mut ser = serde_xml_rs::Serializer::new_from_writer(event_writer);
+    s.serialize(&mut ser)?;
+
+    Ok(String::from_utf8(sink)?)
+}
+
+/// Serializes to a XML string with `ISO-8859-1` encoding attribute.
+pub fn to_iso_string<S: serde::Serialize>(s: S) -> Result<String> {
+    let mut sink = Vec::with_capacity(0xff);
+
+    let event_writer = xml::EventWriter::new_with_config(
+        &mut sink,
+        xml::EmitterConfig::new().pad_self_closing(false),
+    );
+
+    let mut ser = serde_xml_rs::Serializer::new_from_writer(event_writer).with_encoding("ISO-8859-1");
     s.serialize(&mut ser)?;
 
     Ok(String::from_utf8(sink)?)
